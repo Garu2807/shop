d17cfee122aa08e8920af56926b2f356b62ae98f
Garu2807 <arutyunyangagik@gmail.com>
2023-10-01 18:44:10 +0300

    some routes

--- server/routes/api/products.routes.js
+++ server/routes/api/products.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const { Product } = require('../../db/models');
 
@@ -7,4 +8,5 @@ router.get('/', (req, res) => {
     .then((allProducts) => res.json({ products: allProducts }))
     .catch((error) => res.status(500).json(error));
 });
+
 module.exports = router;
